4445a74d38e19f66e9e9301cdca12a582e933611
mickaelg <>
2018-11-26 09:06:14 +0100

    ajout d'une remarque dans le controlleur gestion rentabilité

---
+++ src/controleur/FenFXML_GestionRentabiliteControleur.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import modele.Client;
+
+/**
+ * FXML Controller class
+ *
+ * @author mickael
+ */
+public class FenFXML_GestionRentabiliteControleur implements Initializable {
+    /**
+     * Composants manipulés dans le controlleur
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
